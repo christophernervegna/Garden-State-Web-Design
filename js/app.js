153d965a3bfbbf84c6d2d409302faaa10fe5de61
Chris Nervegna <christophernervegna@gmail.com>
2020-10-24 21:46:51 -0400

    Complete Redesign

--- js/app.js
+++ js/app.js
@@ -5,3 +5,26 @@ let hamburgerMenu = document.querySelector('.hamburger-menu');
 hamburgerMenu.addEventListener('click', function () {
     header.classList.toggle('menu-open');
 });
+
+// Lightbox
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.lb')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
